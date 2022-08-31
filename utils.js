7429896c4619ad2ca8d481d16139eb9da4682d42
raulpoco <72077798+raulpoco@users.noreply.github.com>
2022-08-31 14:24:03 +0300

    Adding the project files

---
+++ utils.js
@@ -0,0 +1,14 @@
+//Setting the Reusable Debounce code
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+
+  };
+};
